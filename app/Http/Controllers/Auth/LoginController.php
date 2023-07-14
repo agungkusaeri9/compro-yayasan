1b571f1634f23032967de4a8cc0a3bf30ca72f9f
Agung Kusaeri <agung.kusaeri9@gmail.com>
2023-07-14 23:55:37 +0700

    update bagian login:

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Setting;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -36,5 +37,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        $setting = Setting::first();
+        return view('auth.login', [
+            'setting' => $setting
+        ]);
     }
 }
